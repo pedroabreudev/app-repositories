03ad885fcdce7a7c1a8ce79f0f3b7d027c05d69f
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-08-18 11:45:40 -0400

    Criando item de Listagem e Menu de Busca

--- app/src/main/java/br/com/dio/app/repositories/ui/MainActivity.kt
+++ app/src/main/java/br/com/dio/app/repositories/ui/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.appcompat.widget.SearchView
 import br.com.dio.app.repositories.R
 import br.com.dio.app.repositories.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
@@ -18,6 +18,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        Log.e(TAG, "onQueryTextSubmit: $query")
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.e(TAG, "onQueryTextChange: $newText" )
+        return false
+    }
+
+    companion object{
+        private const val TAG = "TAG"
     }
 
 }
